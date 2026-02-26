568a4847395076e6041fa66abbfb1b25b8390afe
Viet Anh Tran <vietanhtran.dev@gmail.com>
2026-02-26 17:57:21 +0700

    Fix migration

--- apps/backend/migrations/20260216001306_drop_slug_repositories/migration.sql
+++ apps/backend/migrations/20260216001306_drop_slug_repositories/migration.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "repositories" DROP CONSTRAINT IF EXISTS "repositories_slug_org_id_unique";--> statement-breakpoint
 ALTER TABLE "repositories" DROP COLUMN IF EXISTS "slug";--> statement-breakpoint
-ALTER TABLE "repositories" ADD CONSTRAINT "repositories_name_org_id_unique" UNIQUE("name","org_id");
+ALTER TABLE "repositories" ADD CONSTRAINT IF NOT EXISTS "repositories_name_org_id_unique" UNIQUE("name","org_id");
