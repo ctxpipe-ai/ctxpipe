c6122e766ffcc42c84ee6a33fec0e3b9542cada3
Viet Anh Tran <vietanhtran.dev@gmail.com>
2026-03-02 17:33:45 +0700

    Remove hello world workflow

--- apps/backend/src/openworkflow/hello-world.run.ts
+++
@@ -1,7 +0,0 @@
-import { ow } from "./client.js"
-import { helloWorld } from "./hello-world.js"
-
-const handle = await ow.runWorkflow(helloWorld.spec, {})
-console.log("Workflow run enqueued:", handle.workflowRun.id)
-const result = await handle.result()
-console.log("Workflow completed:", result)
