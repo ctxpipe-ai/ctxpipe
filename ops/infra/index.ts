920072439c480be369ef8ef2bd0717cf8f425388
Viet Anh Tran <vietanhtran.dev@gmail.com>
2026-03-15 22:10:42 +0700

    Got pulumi with railway to work

--- ops/infra/index.ts
+++ ops/infra/index.ts
@@ -1,1 +1,13 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as railway from "@pulumi/railway";
+
+const ctxpipeProject = new railway.Project("ctxpipe", {
+  name: "ctx|",
+});
+
+const backendService = new railway.Service("backend", {
+  projectId: ctxpipeProject.id,
+  name: "backend",
+})
+
+export const railwayProjectId = ctxpipeProject.id;
+export const railwayProjectName = ctxpipeProject.name;
