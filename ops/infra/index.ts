991b65078a3f078eaf2c0ee7450ac700a972a1bc
Viet Anh Tran <vietanhtran.dev@gmail.com>
2026-03-16 11:09:43 +0700

    Import project, services and env into pulumi

--- ops/infra/index.ts
+++ ops/infra/index.ts
@@ -1,13 +1,81 @@
 import * as railway from "@pulumi/railway";
 
-const ctxpipeProject = new railway.Project("ctxpipe", {
-  name: "ctx|",
+const project = new railway.Project("ctx-pipe", {
+    defaultEnvironment: {
+        name: "production",
+    },
+    description: "This is the ctx| application deployed as our SaaS platform",
+    hasPrDeploys: true,
+    name: "ctx| - app",
+    "private": true,
+    workspaceId: "aa3ec44f-f8bd-4beb-bbe0-e4c46e20b14c",
+}, {
+    protect: true,
 });
 
-const backendService = new railway.Service("backend", {
-  projectId: ctxpipeProject.id,
-  name: "backend",
+new railway.Environment("production-env", {
+  name: "production",
+  projectId: project.id,
+}, {
+  protect: true,
+});
+
+new railway.Service("ui", {
+  configPath: "/apps/ui/railway.json",
+  name: "ctx| - ui",
+  projectId: project.id,
+  regions: [{
+      numReplicas: 1,
+      region: "us-east4-eqdc4a",
+  }],
+  sourceRepo: "ctxpipe-ai/ctxpipe",
+  sourceRepoBranch: "main",
+}, {
+  protect: true,
 })
 
-export const railwayProjectId = ctxpipeProject.id;
-export const railwayProjectName = ctxpipeProject.name;
+new railway.Service("backend", {
+  configPath: "/apps/backend/railway.json",
+  name: "ctx| - backend",
+  projectId: project.id,
+  regions: [{
+      numReplicas: 1,
+      region: "us-east4-eqdc4a",
+  }],
+  sourceRepo: "ctxpipe-ai/ctxpipe",
+  sourceRepoBranch: "main",
+}, {
+  protect: true,
+});
+
+new railway.Service("code-search", {
+  configPath: "/apps/codesearch/railway.json",
+  name: "CodeSearch",
+  projectId: project.id,
+  regions: [{
+      numReplicas: 1,
+      region: "us-east4-eqdc4a",
+  }],
+  sourceRepo: "ctxpipe-ai/ctxpipe",
+  sourceRepoBranch: "main",
+  volume: {
+      mountPath: "/data",
+      name: "codesearch-volume-vNK-",
+  },
+}, {
+  protect: true,
+});
+
+new railway.Service("openWorkflow", {
+    configPath: "/apps/backend/railway.worker.json",
+    name: "OpenWorkflow",
+    projectId: project.id,
+    regions: [{
+        numReplicas: 1,
+        region: "us-east4-eqdc4a",
+    }],
+    sourceRepo: "ctxpipe-ai/ctxpipe",
+    sourceRepoBranch: "main",
+}, {
+    protect: true,
+});
